52d6dfc3e61d532ae3a6de8bac81a8c7c33fb516
Cal Page <page.cal@gmail.com>
2022-01-25 15:13:08 -0500

    trial

--- upd.sh
+++ upd.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+pushd site
+../bin/hugo.linux -D
+if [ $? -eq 0 ]; then
+    echo Site Builds - OK
+else
+    echo Site Failed, exiting - FAIL
+fi
+
 git add -A
 git commit -m "trial"
 git push ssh
