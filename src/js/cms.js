8ceeb3dd1d123c4ed1b677fa43ba14935cdc0f3b
Cal Page <page.cal@gmail.com>
2022-01-26 07:43:13 -0500

    rollback a couple of files

--- src/js/cms.js
+++ src/js/cms.js
@@ -8,7 +8,6 @@ import HomePreview from "./cms-preview-templates/home";
 import PostPreview from "./cms-preview-templates/post";
 import ProductsPreview from "./cms-preview-templates/products";
 import ValuesPreview from "./cms-preview-templates/values";
-import ValuesPreview from "./cms-preview-templates/aboutus";
 import ContactPreview from "./cms-preview-templates/contact";
 
 CMS.registerPreviewStyle(styles, { raw: true });
@@ -16,6 +15,5 @@ CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("post", PostPreview);
 CMS.registerPreviewTemplate("products", ProductsPreview);
 CMS.registerPreviewTemplate("values", ValuesPreview);
-CMS.registerPreviewTemplate("aboutus", ValuesPreview);
 CMS.registerPreviewTemplate("contact", ContactPreview);
 CMS.init();
