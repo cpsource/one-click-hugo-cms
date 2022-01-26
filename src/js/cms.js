3d88b886f971002ba16610cb2a48dc1dcee2cd42
Cal Page <page.cal@gmail.com>
2022-01-26 07:26:48 -0500

    now point /values to /aboutus

--- src/js/cms.js
+++ src/js/cms.js
@@ -8,6 +8,7 @@ import HomePreview from "./cms-preview-templates/home";
 import PostPreview from "./cms-preview-templates/post";
 import ProductsPreview from "./cms-preview-templates/products";
 import ValuesPreview from "./cms-preview-templates/values";
+import ValuesPreview from "./cms-preview-templates/aboutus";
 import ContactPreview from "./cms-preview-templates/contact";
 
 CMS.registerPreviewStyle(styles, { raw: true });
@@ -15,5 +16,6 @@ CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("post", PostPreview);
 CMS.registerPreviewTemplate("products", ProductsPreview);
 CMS.registerPreviewTemplate("values", ValuesPreview);
+CMS.registerPreviewTemplate("aboutus", ValuesPreview);
 CMS.registerPreviewTemplate("contact", ContactPreview);
 CMS.init();
